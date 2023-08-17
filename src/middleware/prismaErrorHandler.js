84c7e0db3a5dd39153ca9a9f172fe6fee7186402
OlegChuev <o.chuev@mobidev.biz>
2023-08-17 14:45:38 +0300

    [Feature] Prisma error handler

---
+++ src/middleware/prismaErrorHandler.js
@@ -0,0 +1,32 @@
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '../errors/apiError'
+
+// eslint-disable-next-line import/prefer-default-export
+export const handlePrismaError = (err) => {
+    switch (err.code) {
+        case 'P2002':
+            // handling duplicate key errors
+            return new ApiError(
+                StatusCodes.BAD_REQUEST,
+                `Duplicate field value: ${err.meta.target}`
+            )
+        case 'P2014':
+            // handling invalid id errors
+            return new ApiError(
+                StatusCodes.BAD_REQUEST,
+                `Invalid ID: ${err.meta.target}`
+            )
+        case 'P2003':
+            // handling invalid data errors
+            return new ApiError(
+                StatusCodes.BAD_REQUEST,
+                `Invalid input data: ${err.meta.target}`
+            )
+        default:
+            // handling all other errors
+            return new ApiError(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                `Something went wrong: ${err.message}`
+            )
+    }
+}
